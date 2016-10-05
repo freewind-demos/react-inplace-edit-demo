54ac49114e020f9f0ce6cc1d3e242d660d1f8cf3
Peng.Li <nowind_lee@qq.com>
2016-10-05 15:54:52 +0800

    inplace edit finished

--- hello.jsx
+++ hello.jsx
@@ -1,9 +1,32 @@
 import React, {Component} from 'react';
 
 class Hello extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: 'Double click me to edit!',
+      editing: false
+    }
+  }
 
   render() {
-    return <div>Hello world!</div>;
+    const {editing, content} = this.state;
+    return <div>
+      { editing
+        ? <input type="text" defaultValue={content}
+                 onKeyPress={this._onEnter.bind(this)}/>
+        : <span onDoubleClick={() => this.setState({editing: true})}>{content}</span>}
+    </div>;
+  }
+
+  _onEnter(event) {
+    if (event.charCode === 13) {
+      const newContent = event.target.value;
+      this.setState({
+        content: newContent,
+        editing: false
+      })
+    }
   }
 
 }
